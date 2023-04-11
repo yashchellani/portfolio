b43d49f2660d5f0a858b98376d2821e99456093e
Yash Chellani <35882565+yashchellani@users.noreply.github.com>
2023-04-11 22:02:14 +0800

    updates

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -80,7 +80,7 @@ const ComputersCanvas = () => {
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
-          autoRotate={isMobile ? false : true}
+          autoRotate={isMobile ? true : true}
           rotateSpeed={1}
         />
         <Computers isMobile={isMobile} />
